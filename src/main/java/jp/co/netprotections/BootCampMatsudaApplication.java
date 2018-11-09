f5511cd5a7add12883bc71b4d055bfbd52465faf
tmatsuda@netprotections.co.jp <tmatsuda@netprotections.co.jp>
2018-11-09 13:58:20 +0900

    initial commitment

---
+++ src/main/java/jp/co/netprotections/BootCampMatsudaApplication.java
@@ -0,0 +1,12 @@
+package jp.co.netprotections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampMatsudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampMatsudaApplication.class, args);
+	}
+}
